7196d99ccc957feb75a0ec3c37ec64a046d87e6a
Midland707 <rus14job@ukr.net>
2024-02-19 20:58:14 +0200

    add function for calculation time running of my calcProgram

---
+++ calculations.js
@@ -0,0 +1,22 @@
+// Функція для отримання поточної дати та часу у форматі "день.місяць.рік година:хвилина:секунда"
+function getCurrentDateTime() {
+  const currentDate = new Date();
+  const day = currentDate.getDate().toString().padStart(2, "0");
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+  const year = currentDate.getFullYear();
+  const hours = currentDate.getHours().toString().padStart(2, "0");
+  const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+  const seconds = currentDate.getSeconds().toString().padStart(2, "0");
+  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+}
+const startTime = new Date();
+
+console.log("Початок виконання:", getCurrentDateTime());
+// ########## Початок обчислень ##########
+
+// ########## Кінець обчислень ##########
+const endTime = new Date();
+const executionTime = (endTime - startTime) / 1000;
+
+console.log("Завершення виконання:", getCurrentDateTime());
+console.log("Час виконання (секунди):", executionTime);
